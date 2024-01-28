53eb056e2cad7a33b83b1c6b354a2b9dd35e680b
anshuman2121 <anshuman2121@gmail.com>
2024-01-28 14:11:17 +0100

    commit msg

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+locals {
+  rg_name               = "rg-${var.RESOURCE_NAME_PREFIX}-${var.LOCATION}-${var.ENV}"
+  app_service_plan_name = "plan-${var.RESOURCE_NAME_PREFIX}-${var.LOCATION}-${var.ENV}"
+  app_service_name      = "app-${var.RESOURCE_NAME_PREFIX}-${var.LOCATION}-${var.ENV}"
+  sql_server_name       = "sql-${var.RESOURCE_NAME_PREFIX}-${var.LOCATION}-${var.ENV}"
+  sql_db_name           = "sqldb-${var.RESOURCE_NAME_PREFIX}-${var.LOCATION}-${var.ENV}"
+}
+
+variable "RESOURCE_NAME_PREFIX" {
+  
+}
+
+variable "LOCATION" {
+  
+}
+
+variable "ENV" {
+  default = "dev"  //prod  //sit
+}
